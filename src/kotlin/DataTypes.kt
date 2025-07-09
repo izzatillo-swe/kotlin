2c32c68bfd854c07f8ad892e55d6b6925eab981a
izzatillo-swe <izzatillotilavoldiyev5@gmail.com>
2025-07-09 11:50:19 +0500

    data types added

---
+++ src/kotlin/DataTypes.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var n: Int = 10
+    var l: Long = 100L
+    var d: Double = 3.14
+    var f: Float = 3.14f
+    var b: Boolean = true
+    var s: String = "Hello"
+    var c: Char = 'c'
+}
